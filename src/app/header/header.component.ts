4551c9734e56bd5771b794400467a34e5eb39ea0
Mohaiminul009 <m.moshi009@gmail.com>
2023-02-09 00:28:41 +0600

    home-09/02/23

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../Service/auth.service';
+import { EventBusService } from '../Service/event-bus.service';
+import { StorageService } from '../Service/storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  private roles: string[] = [];
+  isLoggedIn = false;
+  // showAdminBoard = false;
+  // showModeratorBoard = false;
+  username?: string;
+  eventBusSub?: Subscription;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private eventBusService: EventBusService
+  ) {}
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      console.log("USer---",user)
+      this.roles = user.user.role;
+
+      // this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      // this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.user.username;
+    }
+
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.logout();
+    });
+  }
+
+  logout(): void {
+    // this.authService.logout().subscribe({
+    //   next: res => {
+    //     console.log(res);
+    //     this.storageService.clean();
+
+    //     window.location.reload();
+    //   },
+    //   error: err => {
+    //     console.log(err);
+    //   }
+    // });
+
+    this.storageService.clean();
+
+    window.location.reload();
+  }
 }
